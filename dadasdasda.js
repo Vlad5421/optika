1413236d0915bdfcc6df8e12117f100eda0ed6b4
Vladislav <vladislav_ts@list.ru>
2021-03-30 18:13:32 +0600

    second commit

--- dadasdasda.js
+++
@@ -1,4 +0,0 @@
-attachment-post-thumbnail
-console.log(document.querySelector('attachment-post-thumbnail').clientWidth);
-console.log(document.getElementsByClassName('post-text-content').clientWidth);
-document.getElementsByClassName('post-text-content').classList.add
