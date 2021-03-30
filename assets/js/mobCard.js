48228cd33fadd3eb0db89273b8d551774f125959
Vladislav <vladislav_ts@list.ru>
2021-03-30 18:25:45 +0600

    new assets

---
+++ assets/js/mobCard.js
@@ -0,0 +1,7 @@
+if (document.documentElement.clientWidth < 420) {
+    
+    const miniText = document.querySelector('.card_options_smalltext')
+    document.querySelector('.single_variation_wrap').appendChild(miniText)
+
+}
+
